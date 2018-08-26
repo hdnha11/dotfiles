9cbbbc1fc50126954d852e795b3798f592013ea1
Nha Hoang <nha.hoang@trustingsocial.com>
2018-08-26 18:45:13 +0700

    Update install script

--- .bin/dotfiles-install.sh
+++ .bin/dotfiles-install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+. ~/.bin/dotfiles-common.sh
+
+showStep "Clone configuration repository"
 git clone --bare git@github.com:hdnha11/dotfiles.git $HOME/.dotfiles
+
 function dotfiles {
   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
-mkdir -p .dotfiles-backup
+
 dotfiles checkout
+
 if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
-fi;
+  showStep "Checked out config"
+else
+  showStep "Backing up pre-existing dot files"
+  if [ -d .dotfiles-backup ]; then
+    showStep "Remove old backup"
+    rm -R .dotfiles-backup
+  fi
+  mkdir -p .dotfiles-backup
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi
+
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
