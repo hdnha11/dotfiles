81bf0563727290b0e2b4adeb3a08e4e8e994f072
Nha Hoang <hdnha11@gmail.com>
2024-09-10 11:03:23 +0700

    Change color scheme to Nightfox

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -1,4 +1,22 @@
-local colorscheme = 'nordfox'
+local colorscheme = 'nightfox'
+-- local colorscheme = 'nordfox'
+
+require('nightfox').setup {
+  options = {
+    styles = {             -- Style to be applied to different syntax groups
+      comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
+      conditionals = 'NONE',
+      constants = 'NONE',
+      functions = 'italic',
+      keywords = 'NONE',
+      numbers = 'NONE',
+      operators = 'NONE',
+      strings = 'NONE',
+      types = 'italic,bold',
+      variables = 'NONE',
+    },
+  },
+}
 
 local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 if not status_ok then
