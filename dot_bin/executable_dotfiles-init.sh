553b4eb1cd10f388c10579f8aa9ffa1aa3dd8334
Nha Hoang <hdnha11@gmail.com>
2025-01-13 21:17:39 +0700

    Migrate macOS install script Ansible

--- dot_bin/executable_dotfiles-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git init --bare $HOME/.dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dotfiles config --local status.showUntrackedFiles no
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
